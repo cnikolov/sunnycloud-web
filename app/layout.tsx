1b117418ad83c1211a5fb26536c334a90c1c6d81
Christian Nikolov <christian.nikolov@outlook.com>
2023-11-16 13:16:50 +0100

    set default metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: {
     template:
-      '%s | Sunny Cloud Solutions Cloud and Software Development Company',
-    default:'Home'
+      "%s | Sunny Cloud Solutions Cloud and Software Development Company",
+    default:
+      "Home | Sunny Cloud Solutions Cloud and Software Development Company",
   },
   description:
     "Our goal is to provide top-quality services that help our customers save money and achieve their business objectives. We offer a range of consultation services, including website design and development, search engine optimization, and social media marketing.",
